d439f5a3a4a34380c3b2f71266922525279cf06f
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-05 12:34:00 +0900

    Update Prog93.java

--- src/main/java/Prog93.java
+++ src/main/java/Prog93.java
@@ -4,13 +4,13 @@ public class Prog93 {
 
         InvisibleHero h;
         h = new InvisibleHero();
-        System.out.println("勇者" + h.name + "が誕生した! HP:" + h.hp + ", isVisible: " + h.isVisible);
+        System.out.println("勇者" + h.name + "が誕生した！ HP:" + h.hp + ", isVisible: " + h.isVisible);
         h = new InvisibleHero("工太");
-        System.out.println("勇者" + h.name + "が誕生した! HP:" + h.hp + ", isVisible: " + h.isVisible);
+        System.out.println("勇者" + h.name + "が誕生した！ HP:" + h.hp + ", isVisible: " + h.isVisible);
         h = new InvisibleHero("工太", 150);
-        System.out.println("勇者" + h.name + "が誕生した! HP:" + h.hp + ", isVisible: " + h.isVisible);
+        System.out.println("勇者" + h.name + "が誕生した！ HP:" + h.hp + ", isVisible: " + h.isVisible);
         h = new InvisibleHero("工太", 150, false);
-        System.out.println("勇者" + h.name + "が誕生した! HP:" + h.hp + ", isVisible: " + h.isVisible);
+        System.out.println("勇者" + h.name + "が誕生した！ HP:" + h.hp + ", isVisible: " + h.isVisible);
 
     }
 
