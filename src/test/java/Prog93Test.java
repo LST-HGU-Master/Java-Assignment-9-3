2264180bc94985dea560c7ccd492595a4b53187d
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-05 12:34:32 +0900

    Update Prog93Test.java

--- src/test/java/Prog93Test.java
+++ src/test/java/Prog93Test.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import java.io.*;
 
@@ -18,11 +19,19 @@ public class Prog93Test {
         // in.inputln("2"); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         Prog93.main(null);
-
-        // assertion
-        assertEquals("Hello world!\n", bos.toString());
-
         // undo the binding in System
         System.setOut(originalOut);
+        
+        // assertion
+        String[] prints = bos.toString().split("\r\n|\n", -1);
+        try {
+            assertEquals("勇者??が誕生した！ HP:120, isVisible: true", prints[0]);
+            assertEquals("勇者工太が誕生した！ HP:120, isVisible: true", prints[1]);
+            assertEquals("勇者工太が誕生した！ HP:150, isVisible: true", prints[2]);
+            assertEquals("勇者工太が誕生した！ HP:150, isVisible: false", prints[3]);
+        } catch (ArrayIndexOutOfBoundsException excpt) {
+            fail("Prog93.main()のprint出力が4行ではありません!");
+        }
+        assertEquals(5,prints.length,"改行数が5つ以上あります!"); // このif文に到達した時点で prints.lengthは5以上になっている
     }
 }
