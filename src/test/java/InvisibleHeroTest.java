87ec7cef5e92143bb5cce4b930ed7f610e2c3dcd
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-09 16:28:36 +0900

    Update InvisibleHeroTest.java

--- src/test/java/InvisibleHeroTest.java
+++ src/test/java/InvisibleHeroTest.java
@@ -1,7 +1,9 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.io.*;
-
+/**
+  * @version (20220609)
+ */
 public class InvisibleHeroTest {
     //TODO: super()を呼び出しているかはチェックできていない
     
@@ -14,13 +16,15 @@ public class InvisibleHeroTest {
 
         // action
         InvisibleHero h = new InvisibleHero();
-
+        
+        // undo the binding in System
+        System.setOut(originalOut);
+        
         // assertion
         assertEquals(120, h.hp);
         assertEquals("??", h.name);
-
-        // undo the binding in System
-        System.setOut(originalOut);
+        assertEquals(120, h.hp, "InvisibleHero.InvisibleHero()内でのhpの設定が不正です!");
+        assertEquals("??", h.name,"InvisibleHero.InvisibleHero()内でのnameの設定が不正です!");
     }
 
     @Test
@@ -33,12 +37,12 @@ public class InvisibleHeroTest {
         // action
         InvisibleHero h = new InvisibleHero("生命太郎");
 
-        // assertion
-        assertEquals(120, h.hp);
-        assertEquals("生命太郎", h.name);
-
         // undo the binding in System
         System.setOut(originalOut);
+
+        // assertion
+        assertEquals(120, h.hp, "InvisibleHero.InvisibleHero(Strign)内でのhpの設定が不正です!");
+        assertEquals("生命太郎", h.name,"InvisibleHero.InvisibleHero(String)内でのnameの設定が不正です!");
     }
 
     @Test
@@ -51,12 +55,12 @@ public class InvisibleHeroTest {
         // action
         InvisibleHero h = new InvisibleHero("生命太郎", 200);
 
-        // assertion
-        assertEquals(200, h.hp);
-        assertEquals("生命太郎", h.name);
-
         // undo the binding in System
         System.setOut(originalOut);
+
+        // assertion
+        assertEquals(200, h.hp,"InvisibleHero.InvisibleHero(String, int)内でのhpの設定が不正です!");
+        assertEquals("生命太郎", h.name,"InvisibleHero.InvisibleHero(String, int)内でのnameの設定が不正です!");
     }
 
     @Test
@@ -69,12 +73,12 @@ public class InvisibleHeroTest {
         // action
         InvisibleHero h = new InvisibleHero("生命太郎", 300, false);
 
-        // assertion
-        assertEquals(300, h.hp);
-        assertEquals("生命太郎", h.name);
-        assertEquals(false, h.isVisible);
-
         // undo the binding in System
         System.setOut(originalOut);
+
+        // assertion
+        assertEquals(300, h.hp,"InvisibleHero.InvisibleHero(String, int,boolean)内でのhpの設定が不正です!");
+        assertEquals("生命太郎", h.name,"InvisibleHero.InvisibleHero(String, int,boolean)内でのnameの設定が不正です!");
+        assertEquals(false, h.isVisible,"InvisibleHero.InvisibleHero(String, int,boolean)内でのisVisibleの設定が不正です!");
     }
 }
